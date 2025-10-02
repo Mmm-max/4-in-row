dd098af6dd46e879b925b6670374b5d772dcff93
Mmm-max <em.solopov@gmail.com>
2025-10-02 15:49:00 +0300

    add more contex about board tests

--- src/game/BoardTest.java
+++ src/game/BoardTest.java
@@ -1,5 +1,7 @@
 package game;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -7,6 +9,8 @@ import static org.junit.jupiter.api.Assertions.*;
 public class BoardTest {
 
     @Test
+    @Tag("horizontal")
+    @DisplayName("Проверка, что горизонтальная победа работает корректно")
     public void testHorizontalWin() {
         System.out.println("testHorizontalWin");
         Board board = new Board();
@@ -19,6 +23,8 @@ public class BoardTest {
     }
 
     @Test
+    @Tag("vertical")
+    @DisplayName("Проверка, что вертикальная победа работает корректно")
     public void testVerticalWin() {
         System.out.println("testVerticalWin");
         Board board = new Board(7, 6);
@@ -30,6 +36,8 @@ public class BoardTest {
     }
 
     @Test
+    @Tag("diagonal-left-win")
+    @DisplayName("Проверка, что диагональная победа работает корректно")
     public void testDiagonalLeftWin() {
         System.out.println("testDiagonalLeftWin");
         Board board = new Board(7, 6);
@@ -51,6 +59,8 @@ public class BoardTest {
     }
 
     @Test
+    @Tag("diagonal-right")
+    @DisplayName("Проверка, что диагональная победа работает корректно")
     public void testDiagonalRightWin() {
         System.out.println("testDiagonalRightWin");
         Board board = new Board(7, 6);
@@ -69,6 +79,8 @@ public class BoardTest {
     }
 
     @Test
+    @Tag("diagonal-left-win2")
+    @DisplayName("Проверка, что диагональная победа работает корректно")
     public void testDiagonalLeftWin2() {
         System.out.println("testDiagonalLeftWin2");
         Board board = new Board(7, 6);
@@ -93,6 +105,8 @@ public class BoardTest {
     }
 
     @Test
+    @Tag("no-win")
+    @DisplayName("Проверка, что отсутствие победы работает корректно")
     public void testNoWin() {
         System.out.println("testNoWin");
         Board board = new Board(7, 6);
